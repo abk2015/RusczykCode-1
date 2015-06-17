035834cce507ba198eaf5e5ef6be21aa2ee025a5
The Rusczyk Code Developers <RusczykCodeDevelopers@gmail.com>
2015-06-17 16:58:28 -0700

    Update main.RusczykCode.js

--- main.RusczykCode.js
+++ main.RusczykCode.js
@@ -1,4 +1,23 @@
 /*rusczykcode*/
 
 var write = new Function(str){
+  document.write(str);
+}
+var input = new Function(type){
+  x=raw_input();
+  if (type===str){
+    x=str(x)
+  }
+  else if (type===int){
+    x=int(x);
+  }
+  else if (type===int){
+    x=float(x);
+  }
+  else{
+    return null;
+    break;
+  }
+  return x;
+  
 }
